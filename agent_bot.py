b613018b199304074159054c5a878ccc32929584
Morris <itsmorriskiburi@gmail.com>
2025-11-23 21:29:55 +0300

    agent memory

--- agent_bot.py
+++ agent_bot.py
@@ -1,5 +1,5 @@
-from typing import TypedDict, List
-from langchain_core.messages import HumanMessage
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
@@ -7,14 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class AgentState(TypedDict):
-  messages: List[HumanMessage]
+  messages: List[Union[HumanMessage, AIMessage]]
 
 llm = ChatOpenAI(model="gpt-4o")
 
 
 def process(state: AgentState) -> AgentState:
+  """This node will solve the request you input"""
+
   response = llm.invoke(state["messages"])
+
+  state["messages"].append(AIMessage(content=response.content))
   print(f"\nAI: {response.content}")
+  # print("Current state: ", state["messages"])
   return state
 
 graph = StateGraph(AgentState)
@@ -23,5 +28,26 @@ graph.add_edge(START, "process")
 graph.add_edge("process", END)
 agent = graph.compile()
 
+conversation_history = []
+
 user_input = input("Enter: ")
-agent.invoke({"messages": [HumanMessage(content=user_input)]})
+while user_input != 'exit':
+  conversation_history.append(HumanMessage(content=user_input))
+
+  result = agent.invoke({"messages": conversation_history})
+  conversation_history = result["messages"]
+
+  user_input = input('Enter: ')
+
+
+# save contents in a text file for prototyping
+with open("logging.txt", "w") as file:
+  file.write("Your conversation Log: \n")
+  for message in conversation_history:
+    if isinstance(message, HumanMessage):
+      file.write(f'You: {message.content} \n')
+    elif isinstance(message, AIMessage):
+      file.write(f'AI: {message.content}\n\n')
+  file.write("End of Conversation")
+
+print('Conversation save to logging.txt')
